5de0ce1225fd469bb006209cd20bfdcbcebf65a2
gurcancekic <arinyazilim@gmail.com>
2024-06-20 10:54:35 +0300

    service model and register to admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Service
 
-# Register your models here.
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'learn_more_link')
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ('title',)
+
--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Service(models.Model):
+    title = models.CharField(max_length=200, blank=False, null=False)
+    image_url = models.URLField(max_length=200)
+    description = models.TextField()
+    learn_more_link = models.URLField(max_length=200, blank=True)
+    slug = models.SlugField(unique=True, blank=False)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
