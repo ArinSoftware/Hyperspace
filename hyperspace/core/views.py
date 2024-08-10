1c5b32ae00c159e693748f84e8ffe482ceeba085
gurcancekic <arinyazilim@gmail.com>
2024-08-10 23:40:43 +0300

    Lesson04

--- hyperspace/core/views.py
+++ hyperspace/core/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Welcome to Hyperspace")
+    return render(request, 'core/index.html')
