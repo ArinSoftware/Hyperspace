26e96383fb8cf797e98259d24c6c871f25b5f319
gurcancekic <arinyazilim@gmail.com>
2024-08-24 16:15:57 +0300

    Lesson 07

--- hyperspace/core/views.py
+++ hyperspace/core/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from datetime import datetime
 
 def index(request):
-    return render(request, 'core/index.html')
+
+    static_content = """
+    		<div class="inner">
+			<h1>Hyperspace</h1>
+			<p>Just another fine responsive site template designed by <a href="http://html5up.net">HTML5 UP</a><br />
+			and released for free under the <a href="http://html5up.net/license">Creative Commons</a>.</p>
+			<ul class="actions">
+				<li><a href="#one" class="button scrolly">Learn more</a></li>
+			</ul>
+		</div>
+    """
+
+    context = {
+        'static_content': static_content
+    }
+
+    return render(request, 'core/index.html', context)
