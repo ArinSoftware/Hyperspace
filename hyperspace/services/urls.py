22421f3a25fb313dd0e2e819df4100527e7cc191
gurcancekic <arinyazilim@gmail.com>
2024-09-15 01:37:08 +0300

    Lesson 11

--- hyperspace/services/urls.py
+++ hyperspace/services/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.service_list, name='service_list')
+    path('', views.service_list, name='service_list'),
+    path('<slug:slug>/', views.service_detail, name="service_detail")
 ]
--- hyperspace/services/views.py
+++ hyperspace/services/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . models import Service
 
 # Create your views here.
@@ -6,3 +6,8 @@ def service_list(request):
     services = Service.objects.all()
 
     return render(request, 'services/service_list.html', {'services': services})
+
+def service_detail(request, slug):
+    service = get_object_or_404(Service, slug=slug)
+
+    return render(request, 'services/service_detail.html', {'service': service})
