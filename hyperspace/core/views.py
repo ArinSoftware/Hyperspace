41d749cf06de05b49d71dea73a640d00ac49648d
gurcancekic <arinyazilim@gmail.com>
2024-10-12 20:26:46 +0300

    Lesson 15

--- hyperspace/core/views.py
+++ hyperspace/core/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.core.mail import send_mail
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from services.models import Service
 from blog.models import Blog
 
@@ -26,3 +31,23 @@ def index(request):
     }
 
     return render(request, 'core/index.html', context)
+
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        subject = f"Hyperspace Contact Form"
+        message_body = f"Message from {name} - {email}: \n\n {message}"
+        recipient_list = [settings.EMAIL_HOST_USER]
+
+        try:
+            send_mail(subject, message_body, email, recipient_listx)
+        except Exception:
+            messages.error(request, "Something happened")
+        else:
+            messages.success(request, "We have received your email")
+
+    return HttpResponseRedirect(reverse('index') + '#three')
--- hyperspace/hyperspace/settings.py
+++ hyperspace/hyperspace/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +129,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
